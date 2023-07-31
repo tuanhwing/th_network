e5dd6e4cfef0dac746de0cbf4c345be62ea67aca
Tuấn Hwing <tuanhwing2014@gmail.com>
2023-07-31 19:49:15 +0700

    feat : create class THErrorResponse contains error_code + errors

--- lib/network/th_response.dart
+++ lib/network/th_response.dart
@@ -4,10 +4,30 @@ import 'package:th_dependencies/th_dependencies.dart';
 
 import '../common/common.dart';
 
+class THErrorResponse {
+  THErrorResponse({
+    this.errorCode, 
+    this.errors});
+  final int? errorCode;
+  final List<String>? errors;
+
+  factory THErrorResponse.fromJson(Map<String, dynamic> json) {
+    return THErrorResponse(
+      errorCode: json['error_code'] ?? 0,
+      errors: List<String>.from(json['errors'] ?? <String>[]),
+    );
+  }
+}
+
 class THResponse<T> {
-  THResponse({this.code = 200, this.status = false, this.data, this.message});
+  THResponse({
+    this.code = 200, 
+    this.status = false, 
+    this.error, this.data, 
+    this.message});
   int? code;
   bool? status;
+  THErrorResponse? error;
   T? data;
   String? message;//error message
 
@@ -20,6 +40,7 @@ class THResponse<T> {
       return THResponse(
         code: response.statusCode,
         status: response.data['status'],
+        error: THErrorResponse.fromJson(response.data['error'] ?? <String, dynamic>{}),
         data: response.data['data'],
         message: response.data['message']
       );
